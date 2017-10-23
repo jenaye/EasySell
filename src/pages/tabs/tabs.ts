a7ed9f4c4d8dbb236844717b10998f6cc52bef68
Houziaux mike <Stahlilistenvoid@gmail.com>
2017-10-23 22:08:56 +0200

    add product page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ProduitsPage } from '../produits/produits';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +11,9 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = ProduitsPage;
   tab3Root = ContactPage;
+  tab4Root = AboutPage;
 
   constructor() {
 
