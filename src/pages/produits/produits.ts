76d2370df8fdd563f82d91d7b03d1ec4f518bafa
Houziaux mike <Stahlilistenvoid@gmail.com>
2017-10-25 20:57:16 +0200

    adding provider

--- src/pages/produits/produits.ts
+++ src/pages/produits/produits.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProduitProvider } from '../../providers/produit/produit'
 
 /**
  * Generated class for the ProduitsPage page.
@@ -14,36 +15,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'produits.html',
 })
 export class ProduitsPage {
+  private products = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private productprovider: ProduitProvider
+  ) {
   }
 
   ionViewDidLoad() {
+    this.productprovider.getAllProduct().subscribe(produits => {
+      console.log(produits)
+        this.products = produits;
+    });
     console.log('ionViewDidLoad ProduitsPage');
   }
-
-    items = [
-        'Pokémon Yellow',
-        'Super Metroid',
-        'Mega Man X',
-        'The Legend of Zelda',
-        'Pac-Man',
-        'Super Mario World',
-        'Street Fighter II',
-        'Half Life',
-        'Final Fantasy VII',
-        'Star Fox',
-        'Tetris',
-        'Donkey Kong III',
-        'GoldenEye 007',
-        'Doom',
-        'Fallout',
-        'GTA',
-        'Halo'
-    ];
-
-    itemSelected(item: string) {
-        console.log("Selected Item", item);
-    }
 
 }
