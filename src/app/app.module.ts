531241869538600e134e62427c13df426b54d097
Qcdu59 <quentin.alcb.59@hotmail.fr>
2017-10-25 22:08:53 +0200

    adding product summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { FicheproduitPage } from "../pages/ficheproduit/ficheproduit";
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -15,6 +16,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
+      FicheproduitPage,
     AboutPage,
     ContactPage,
     HomePage,
@@ -28,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+      FicheproduitPage,
     AboutPage,
     ContactPage,
     HomePage,
