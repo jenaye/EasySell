6094faa1872c864dbd4ca9d357117d149d048fb6
Houziaux mike <Stahlilistenvoid@gmail.com>
2017-10-25 21:19:02 +0200

    add .env to API

--- src/providers/produit/produit.ts
+++ src/providers/produit/produit.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Response, Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { ENV } from '../../../src/config/env';
 
 
 @Injectable()
 export class ProduitProvider {
 
   constructor(public http: Http) {
+      console.log(ENV.API_URL)
   }
 
   getAllProduct() {
       return this.http
-          .get('http://api.oipnet.info/produits.json')
+          .get(ENV.API_URL+ '/produits.json')
           .map((res: Response) => {
               return res.json();
           });
